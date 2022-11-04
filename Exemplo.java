8192e532feecd4c1de476795a0263297778da6b9
thiagoflins <thiagofeitosalins@gmail.com>
2022-11-03 21:08:47 -0300

    branchLorenna

--- Exemplo.java
+++ Exemplo.java
@@ -1,6 +1,6 @@
 public class Exemplo{
     public static void main(String[] args) {
-        System.out.println("Olá Lorenna");
-        System.out.println("Ola Michelle");
+        System.out.println("Lore Joga volei");
+        
     }
 }
